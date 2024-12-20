800deb873bb6ceba771e3233af899170dfb1c9ed
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-12-20 15:12:05 +0530

    molbile and tablete responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Earls Pizza | Pizza Delivery Service</title>
-        <meta name="description" content="Earls Pizza is a pizza delivery service that delivers fresh, delicious pizzas to your doorsteps. We use the best ingredients and techniques to ensure that your pizza arrives hot and ready to eat. Our pizzas are made with love and care, and we are committed to providing the best service possible. Whether you're in the mood for a classic margherita, a spicy pepperoni, or a vegetarian option, we have something to satisfy every palate. Come try us out and experience the best pizza delivery service in town!" />
+        <title>Earls Pizza | Pizza Restaurant and Delivery Service</title>
+        <meta name="description" content="Earls Pizza is a pizza restaurant and delivery service that delivers fresh, delicious pizzas to your doorsteps. We use the best ingredients and techniques to ensure that your pizza arrives hot and ready to eat. Our pizzas are made with love and care, and we are committed to providing the best service possible. Whether you're in the mood for a classic margherita, a spicy pepperoni, or a vegetarian option, we have something to satisfy every palate. Come try us out and experience the best pizza delivery service in town!" />
         <link rel="icon" href="./favicon.ico" />
       </head>
       <body className={inter.className}>
